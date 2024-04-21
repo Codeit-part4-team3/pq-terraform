4a0fe018e0ff116c04e8030ef5ee242caeef89ac
ohddang <ohddang509@gmail.com>
2024-04-22 04:24:13 +0900

    feat: nat gateway, igw 연결

--- vpc.tf
+++ vpc.tf
@@ -37,3 +37,70 @@ resource "aws_security_group" "pq-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc-pq.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc-pq.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "igw-rt"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnet["public-subnet"].id
+}
+
+resource "aws_route_table" "chat" {
+  vpc_id = aws_vpc.vpc-pq.id
+
+  route {
+    cidr_block = "10.0.0.0/24"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "chat-subnet-rt"
+  }
+}
+
+resource "aws_route_table" "user" {
+  vpc_id = aws_vpc.vpc-pq.id
+
+  route {
+    cidr_block = "10.0.0.0/24"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "user-subnet-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.subnet["public-subnet"].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "user" {
+  subnet_id      = aws_subnet.subnet["user-subnet"].id
+  route_table_id = aws_route_table.user.id
+}
+
+resource "aws_route_table_association" "chat" {
+  subnet_id      = aws_subnet.subnet["chat-subnet"].id
+  route_table_id = aws_route_table.chat.id
+}
